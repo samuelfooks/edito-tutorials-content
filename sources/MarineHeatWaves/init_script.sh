d632ea0ed26c050635624b880a18865290dbdd39
rdussurget <rdussurget@mercator-ocean.fr>
2024-03-27 09:34:22 +0000

    model lab cleanup

--- sources/MarineHeatWaves/init_script.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-pip install copernicus-marine-client cartopy regionmask
-
-git clone --depth 1 --branch master --no-checkout https://${GIT_USER_NAME}:${GIT_PERSONAL_ACCESS_TOKEN}@gitlab.mercator-ocean.fr/pub/edito-infra/edito-tutorials-content/
-cd ~/work/edito-tutorials-content/
-git sparse-checkout set sources/MarineHeatWaves
-git checkout master
-cd sources/MarineHeatWaves
